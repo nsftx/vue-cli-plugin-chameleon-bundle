5ec2cca94308f74311763f8758b71fe2930ff18d
Lana <dzidic.lana@nsoft.com>
2018-09-07 16:41:23 +0200

    Add front matter to doc file

--- docs.js
+++ docs.js
@@ -5,4 +5,9 @@ const srcPath = path.resolve('./README.md');
 const baseDir = path.dirname(__dirname).split('vue-cli-plugin-chameleon-bundle')[0];
 const destPath = path.resolve(baseDir, 'chameleon-docs/developers/bundles', 'vue-cli-plugin.md')
 
-fs.createReadStream(srcPath).pipe(fs.createWriteStream(destPath));
+const fileContent = fs.readFileSync(srcPath).toString();
+const frontMatter = `---
+title: "Vue CLI Bundle Plugin"
+---\n\n`;
+
+fs.writeFileSync(destPath, `${frontMatter}${fileContent}`);
